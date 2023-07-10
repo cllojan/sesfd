c4e7987888171916321dcfc907a8eddfd0fca067
Aikawa <cesar.lojan8@gmail.com>
2023-07-10 17:04:43 +0000

    category, min, max

--- components/FilterProduct.js
+++ components/FilterProduct.js
@@ -3,40 +3,65 @@ import {useRouter} from 'next/router';
 
 
 export default function FilterProduct({category}) {
+    const [categoriasSeleccionadas, setCategoriasSeleccionadas] = useState([]);
+    const [searchTerm, setSearchTerm] = useState(" ");
+    const [priceMin, setPriceMin] = useState(0);
+    const [priceMax, setPriceMax] = useState(1000);
     const router = useRouter();
     
     const listCategorias = []
     const handleChange = (e) => {
-        listCategorias.push(e.target.value)            
+      const categoriaSeleccionada = e.target.value;     
+      console.log(e.target.dataset.parentId);
+      if(categoriasSeleccionadas.includes(categoriaSeleccionada)){
+        let inxC = categoriasSeleccionadas.indexOf(categoriaSeleccionada)
+        categoriasSeleccionadas.splice(inxC,1);
+      } else{
+        setCategoriasSeleccionadas([...categoriasSeleccionadas, categoriaSeleccionada]);        
+      }
+      
     };   
+
     const handleSearch = (event) => {    
         event.preventDefault();
-        let a = listCategorias.join(",")
-        console.log(a);
-        router.push({
-          pathname:`/product/search/amd?category=${a}`
-        });
+        
+        const categoriasQuery = categoriasSeleccionadas.join(',');
+        console.log(categoriasQuery)
+        router.push(`/product/search/${searchTerm}?categorys=${categoriasQuery}&min=${priceMin}&max=${priceMax}`);
         
       };
     return (
         <div className="">
             <form action="" onSubmit={handleSearch}>
-                <input type="text" name="search" />
+                <input type="text" name="search" onChange={(e)=>setSearchTerm(e.target.value)}/>
                 <label htmlFor="">Menor</label>
-                <input type="text" name="menor" />
+                <input type="text" name="menor" value={priceMin} onChange={(e)=>setPriceMin(e.target.value)}/>
                 <label htmlFor="">Mayor</label>
-                <input type="text" name="mayor" />
+                <input type="text" name="mayor" value={priceMax} onChange={(e)=>setPriceMax(e.target.value)}/>
                 <div className="container_ca">
                     {
                         category?.map((elm,inx) => (
                             <div className="" key={inx} >
-                                <input type='checkbox' value={elm.name} onChange={e=>handleChange(e)}/>
+                                <input 
+                                type='checkbox' 
+                                value={elm.id} 
+                                onChange={e=>handleChange(e)} 
+                                
+                                />
+                               
                                 <label htmlFor="">{elm.name}</label>
                                 {
                                     elm.parents.length >0?                                    
                                         elm.parents.map((em,i) =>(
-                                            <div key={i} className=" cont" style={{marginLeft:"30px"}}>                                                
-                                                <input type='checkbox' value={em.name} onChange={e=>handleChange(e)} checked/>
+                                            <div key={i} className="cont" style={{marginLeft:"30px"}}>                                                
+                                                <input 
+                                                    type='checkbox' 
+                                                    value={em.id}
+                                                    
+                                                    onChange={e=>handleChange(e)}
+                                                    checked={categoriasSeleccionadas.includes(elm.name)}
+                                                    data-parent-id={elm.name}
+                                                    />
                                                 <label htmlFor="">{em.name}</label>
                                             </div>
                                             ))
--- pages/product/search/[query].js
+++ pages/product/search/[query].js
@@ -1,83 +1,100 @@
 import Header from "@/components/Header";
 import styled from "styled-components";
 import Center from "@/components/Center";
-import {mongooseConnect} from "@/lib/mongoose";
-import {Product} from "@/models/Product";
-import {Category }from '@/models/Category';
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+import { Category } from '@/models/Category';
 import ProductsGrid from "@/components/ProductsGrid";
 import Title from "@/components/Title";
 import mongoose from "mongoose";
 import FilterProduct from '@/components/FilterProduct';
 
 
-export default function SearchProduct({products,category}){
-  
-    return (
-        <>
-          <Header />
-        
-          <Center>
-            
-            <Title>All products</Title>
-            <div>
-            <FilterProduct category={category}></FilterProduct>
-              <ProductsGrid products={products} />
-            </div>
-            
-          </Center>
-        </>
-      );
+export default function SearchProduct({ products, category }) {
+
+  return (
+    <>
+      <Header />
+
+      <Center>
+
+        <Title>All products</Title>
+        <div>
+          <FilterProduct category={category}></FilterProduct>
+          <ProductsGrid products={products} />
+        </div>
+
+      </Center>
+    </>
+  );
 }
 export async function getServerSideProps(context) {
-     await mongooseConnect();
-     const {query} = context.query
-     const categoryIds = ["6490d47f07756020e23836d9", "64a6e321baa8f76629dab656"];
-     
-     
-    const validCategoryIds = categoryIds.filter((categoryId) =>
+  await mongooseConnect();
+  const { query, categorys,min,max } = context.query
+
+  const categoryIds = categorys?.length > 0 ? categorys.split(",") : categorys ;    
+  console.log(context);
+  const productsQuery = {
+    $or: [
+      { title: { $regex: query, $options: "i" } },
+      { description: { $regex: query, $options: "i" } },
+    ],    
+  };  
+  if (categoryIds?.length > 0) {
+    const validCategoryIds = categoryIds?.filter((categoryId) =>
       mongoose.Types.ObjectId.isValid(categoryId)
     );
+    productsQuery.category = { $in: validCategoryIds };
+  }
+  if(min && max ){
+    productsQuery.price = { $gte: min, $lte: max };
+  }
 
-     const products = await Product.find({
-      $and:[
-        {
-          $or: [
-            { title: { $regex: query, $options: "i" } },
-            { description: { $regex: query, $options: "i" } },
-            
-          ],
+  const products = await Product.find(productsQuery);
+  /**
+   * 
+   *  $and:[
+     {
+       $or: [
+         { title: { $regex: query, $options: "i" } },
+         { description: { $regex: query, $options: "i" } },
+         
+       ],
 
-        },
-        {category:{$in:validCategoryIds}},
-        { price: { $gte: 100, $lte: 250 }}
-      ]
-        
-      });
+     },
+     {category:{$in:validCategoryIds}},
+     { price: { $gte: 100, $lte: 250 }}
+   ]
+     
+   });
+   */
 
-    //const product = await db.collection("products")        
-    //const category = await Category.find().populate('parent')
-    const category = await Category.find({ parent: { $exists: false } }, 'name');
-    const categoriasHijas = {};
-    
-    for (const categoriaPrincipal of category) {
-      const categoriaId = categoriaPrincipal._id;
-      const hijas = await Category.find({ parent: categoriaId }, 'name');
-      categoriasHijas[categoriaId] = hijas;
-    }
-    
-    const categoriasJson = category.map((categoriaPrincipal) => ({
-      id: categoriaPrincipal._id,
-      name: categoriaPrincipal.name,
-      parents: categoriasHijas[categoriaPrincipal._id] || []
-    }));
-    for (const categoria of categoriasJson) {
-      categoria.parents = categoria.parents.filter((hija) => !categoriasJson.some((c) => c.id === hija.id));
-    }
-    
-    return {
-      props: {
-        products: JSON.parse(JSON.stringify(products)),
-        category:JSON.parse(JSON.stringify(categoriasJson))
-      }
+
+
+  //const product = await db.collection("products")        
+  //const category = await Category.find().populate('parent')
+  const category = await Category.find({ parent: { $exists: false } }, 'name');
+  const categoriasHijas = {};
+
+  for (const categoriaPrincipal of category) {
+    const categoriaId = categoriaPrincipal._id;
+    const hijas = await Category.find({ parent: categoriaId }, 'name');
+    categoriasHijas[categoriaId] = hijas;
+  }
+
+  const categoriasJson = category.map((categoriaPrincipal) => ({
+    id: categoriaPrincipal._id,
+    name: categoriaPrincipal.name,
+    parents: categoriasHijas[categoriaPrincipal._id] || []
+  }));
+  for (const categoria of categoriasJson) {
+    categoria.parents = categoria.parents.filter((hija) => !categoriasJson.some((c) => c.id === hija.id));
+  }
+
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+      category: JSON.parse(JSON.stringify(categoriasJson))
     }
   }
+}
