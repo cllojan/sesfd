73c4ea8ddd1412595d9a098da3e2681cb350f2d6
Aikawa <cesar.lojan8@gmail.com>
2023-12-01 18:20:31 +0000

    featured

--- components/Featured.js
+++ components/Featured.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 const HeroBannerWrapper = styled.div`
-  background-image: url("https://th.bing.com/th/id/R.44290ccaf1b4706c84325884fadba42b?rik=9Xz5Uhj7DNLtkw&riu=http%3a%2f%2fgalaxygamingnews.com%2fwp-content%2fuploads%2f2021%2f11%2fshutterstock_1753061831.jpg&ehk=EsT8gTxn0ng%2foQsHiv3BxORuQFrRDxmFoONuVTx%2f4W8%3d&risl=&pid=ImgRaw&r=0");
+  background-image: url("https://images.shopcdn.co.uk/ef/10/ef108b7f84bbd94f610326d7edb18dd5/1024x1024/webp/fit?quality=100");
   background-size: cover;
-  background-position: center;
-  height: 400px; /* Ajusta la altura según tus necesidades */
+  
+  height: 500px; /* Ajusta la altura según tus necesidades */
   display: flex;
   align-items: center;
   justify-content: center;
